ff0086af0c6bdca15e7c7c6f7d26c2ff44486d3f
MDC <markdcross@gmail.com>
2020-09-22 10:25:10 -0400

    Functionized local storage instead of hard coding

--- assets/scripts.js
+++ assets/scripts.js
@@ -22,17 +22,6 @@ $(document).ready(function () {
         '5:00 PM',
     ];
 
-    // Set local storage variables
-    var storedItems_9 = localStorage.getItem(storedItems_9) || [];
-    var storedItems_10 = localStorage.getItem(storedItems_10) || [];
-    var storedItems_11 = localStorage.getItem(storedItems_11) || [];
-    var storedItems_12 = localStorage.getItem(storedItems_12) || [];
-    var storedItems_13 = localStorage.getItem(storedItems_13) || [];
-    var storedItems_14 = localStorage.getItem(storedItems_14) || [];
-    var storedItems_15 = localStorage.getItem(storedItems_15) || [];
-    var storedItems_16 = localStorage.getItem(storedItems_16) || [];
-    var storedItems_17 = localStorage.getItem(storedItems_17) || [];
-
     //---------------
     // Loop to make table based on length of hours array
     //---------------
@@ -43,7 +32,7 @@ $(document).ready(function () {
         var tRow = $('<tr class="hourColor">');
         // Populate first column with hours array along with dataHour
         var hourRow = $(
-            `<td class="align-middle"><h3 class="time" data-hour="${dataHour}">${hours[i]}</h3></td>`
+            `<td class="align-middle"><h3 class="time" id="${hours[i]}" data-hour="${dataHour}">${hours[i]}</h3></td>`
         );
         // Create a column with a textbox along with dataHour
         var task = $(
@@ -58,6 +47,31 @@ $(document).ready(function () {
         // Append the table row to the table body
         $('tbody').append(tRow);
     }
+
+    //---------------
+    // Add content of text area to local storage on save, and display
+    //---------------
+    // Index the textareas in the html, and assigns local storage values
+    init();
+    function init() {
+        for (var k = 9; k < 18; k++) {
+            // Populates textareas with existing local storage
+            $('#' + k + 'text').val(localStorage.getItem(k));
+        }
+    }
+
+    // Event listener on save button
+    $('.saveBtn').click(function (e) {
+        e.preventDefault();
+        // Identifies what row is being saved to local storage
+        var id = $(this).data('hour');
+        // Saves the row and todo item into local storage
+        let task = {
+            hour: $(this).data('hour'),
+            message: $('#' + id + 'text').val(),
+        };
+        localStorage.setItem(task.hour, task.message);
+    });
 
     //---------------
     // Color code the hour blocks based on time of day and whether is in past, present, or future
@@ -83,72 +97,4 @@ $(document).ready(function () {
                 .css('background-color', '#77dd77');
         }
     }
-
-    //---------------
-    // Add content of text area to local storage on save, and display
-    //---------------
-    // save button click event 
-    //TODO Figure out how to loop the local storage!!
-    $('.saveBtn').on('click', function (e) {
-        e.preventDefault();
-
-        // Use data-hour to drive logic around what text areas to populate
-        var id = $(this).data('hour');
-
-        // Set the new data items
-        var task = {
-            hour: $(this).data('hour'),
-            message: $('#' + id + 'text').val(),
-        };
-
-        if ($(this).data('hour') === 9) {
-            storedItems_9.push(task);
-            localStorage.setItem('9', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 10) {
-            storedItems_10.push(task);
-            localStorage.setItem('10', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 11) {
-            storedItems_11.push(task);
-            localStorage.setItem('11', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 12) {
-            storedItems_12.push(task);
-            localStorage.setItem('12', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 13) {
-            storedItems_13.push(task);
-            localStorage.setItem('13', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 14) {
-            storedItems_14.push(task);
-            localStorage.setItem('14', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 15) {
-            storedItems_15.push(task);
-            localStorage.setItem('15', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 16) {
-            storedItems_16.push(task);
-            localStorage.setItem('16', JSON.stringify(task.message));
-        }
-        if ($(this).data('hour') === 17) {
-            storedItems_17.push(task);
-            localStorage.setItem('17', JSON.stringify(task.message));
-        }
-    });
-
-    // Pull local storage for display
-    window.onload = function () {
-        $('#9text').val(JSON.parse(localStorage.getItem('9')));
-        $('#10text').val(JSON.parse(localStorage.getItem('10')));
-        $('#11text').val(JSON.parse(localStorage.getItem('11')));
-        $('#12text').val(JSON.parse(localStorage.getItem('12')));
-        $('#13text').val(JSON.parse(localStorage.getItem('13')));
-        $('#14text').val(JSON.parse(localStorage.getItem('14')));
-        $('#15text').val(JSON.parse(localStorage.getItem('15')));
-        $('#16text').val(JSON.parse(localStorage.getItem('16')));
-        $('#17text').val(JSON.parse(localStorage.getItem('17')));
-    };
 });
